961100f4d0f4a7b03eed97bd18db52190880084d
iyinraphael <iyinraphael@gmail.com>
2019-03-11 00:11:48 -0700

    initial commit

---
+++ StarWars Search Swift-ObjC/LSIPerson.m
@@ -0,0 +1,13 @@
+//
+//  LSIPerson.m
+//  StarWars Search Swift-ObjC
+//
+//  Created by Iyin Raphael on 3/11/19.
+//  Copyright © 2019 Iyin Raphael. All rights reserved.
+//
+
+#import "LSIPerson.h"
+
+@implementation LSIPerson
+
+@end
